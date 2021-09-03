3443c1d2771e3d84e56683cdaf4883c3273ce5d8
DizzyReading <daintybeat@gmail.com>
2021-09-03 21:08:37 +0530

    minor

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Covid-19 Tracker",
     description:
       "Built with React.js, get real time Covid-19 cases across the globe with the added functionality of fetching historical as well as current data through API's.",
-    image: "/images/1.png",
+    image: "/images/1.jpeg",
     tags: ["React"],
     source: "https://google.com",
     visit: "https://nostalgic-feynman-b6f6d1.netlify.app/",
@@ -13,7 +13,7 @@ export const projects = [
     title: "MERN E-Commerce",
     description:
       "Built with the MERN stack, this shopping cart utilizes state mangament library Redux with the usage of JSON Web Tokens for Authentication and Payment gateways like Paypal.",
-    image: "/images/2.png",
+    image: "/images/2.jpeg",
     tags: ["Mongo", "Express", "React", "Node", "Redux"],
     source: "https://google.com",
     visit: "https://my-mern-stack-app.herokuapp.com/",
@@ -23,7 +23,7 @@ export const projects = [
     title: "Travel Guru",
     description:
       "Built with React, get all the best restaurants, hotels, & attractions with the implementation of Google Maps & data collection through API's.",
-    image: "/images/3.png",
+    image: "/images/3.jpeg",
     tags: ["React", "Google Maps"],
     source: "https://google.com",
     visit: "https://wonderful-rosalind-c66cc3.netlify.app/",
